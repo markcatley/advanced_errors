80468e051bee6079bd293ebaf8d98d41e1b7eefe
Mark Catley <mark@nexx.co.nz>
2008-08-05 20:54:27 +1200

    Reorganising things

---
+++ lib/advanced_errors/full_messages.rb
@@ -0,0 +1,18 @@
+module Nexx::AdvancedErrors::FullMessages
+  def full_messages_with_ignore_attribute
+    error = ActiveRecord::Errors.new @base
+    self.each do |attribute, message|
+      unless message.match(/^\^/)
+        error.add attribute, message
+      else
+        error.add_to_base message[1..message.length] #Adding to base will mean that the attribute will not be displayed
+      end
+    end
+    error.full_messages_without_ignore_attribute
+  end
+  
+  def self.included(base)
+    base.class_eval { alias_method :full_messages_without_ignore_attribute, :full_messages }
+    base.class_eval { alias_method :full_messages, :full_messages_with_ignore_attribute }
+  end
+end
